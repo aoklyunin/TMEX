327f467d810cbafa1e423724711f789f2b876011
alex <aoklyunin@gmail.com>
2018-03-02 13:02:50 +0300

    weaaasdqdsafя

--- tmex_main/wsgi.py
+++ tmex_main/wsgi.py
@@ -13,9 +13,10 @@ from django.core.wsgi import get_wsgi_application
 
 from whitenoise.django import DjangoWhiteNoise
 
-sys.path.append('/opt/bitnami/apps/django/django_projects/sc')
-os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/sc/egg_cache")
+#sys.path.append('/opt/bitnami/apps/django/django_projects/sc')
+#os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/sc/egg_cache")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tmex_main.settings.production")
+
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
